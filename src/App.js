bf74a6e13096f43c0bfa6d78f0d435fa90ac80ed
ShravaniSiripuram <[ssiripuram46@gmail.com]>
2025-01-02 15:51:43 +0530

    updated the app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React along with 
+          Learn React along with me and code!!
         </a>
       </header>
     </div>
